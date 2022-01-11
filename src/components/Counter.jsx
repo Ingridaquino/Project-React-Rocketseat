7bcd1a311c9f3065716cf1b280843fa74519a0a4
Ingrid Aquino <ingridaquinosoares@live.com>
2022-01-11 15:04:24 -0300

    Primeiros passos

---
+++ src/components/Counter.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+
+// hook -> gancho
+
+export function Counter () {
+   const [counter, setCounter] = useState(0);
+
+    //[let] = Let it chance -> deixe isso mudar
+
+    function increment () {
+        setCounter(counter + 1);
+    }
+    return (
+        <div>
+            <h2>{counter}</h2>
+            <button type="button" onClick={increment}>Increment</button>
+        </div>
+    )
+}
