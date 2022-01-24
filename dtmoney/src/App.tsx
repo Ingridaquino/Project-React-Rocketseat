7b96767bfdbce4b0bfac1f31fb6faf42e07ee600
Ingrid Aquino <ingridaquinosoares@live.com>
2022-01-24 15:56:41 -0300

    Add o Dashboard e o summary

--- dtmoney/src/App.tsx
+++ dtmoney/src/App.tsx
@@ -1,10 +1,13 @@
 import { Header } from "./components/Header/Header";
+import { Dashboard } from "./Dashboard";
+
 import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
     <>
         <Header />
+        <Dashboard />
         <GlobalStyle />   
     </>
   );
