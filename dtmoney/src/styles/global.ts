5f5499aece745250c4f611689afb239048c1518d
Ingrid Aquino <ingridaquinosoares@live.com>
2022-01-24 14:19:54 -0300

    Adicionando projeto

---
+++ dtmoney/src/styles/global.ts
@@ -0,0 +1,42 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+    :root {
+      --background:#f0f2f5;
+    }
+
+
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+
+    body{
+      background: var(--background);
+      -webkit-font-smoothing:antialiased; // Para as fonts ficarem detalhadas
+    }
+
+
+    html {
+      @media (max-width: 1080px) {
+        font-size: 93.75%;
+      }
+
+      @media (max-width: 720px) {
+        font-size:81.5%;
+      }
+    }
+
+
+
+
+
+
+
+
+
+
+`
+
